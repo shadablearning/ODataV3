6f342855284616ae0252dd572825d5219dfcab26
shadablearning <51901767+shadablearning@users.noreply.github.com>
2019-06-26 12:20:22 +0530

    xcxc

---
+++ PrototypeODataV3/ProductService/App_Start/WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using ProductService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.OData.Extensions;
+using System.Web.Http;
+using System.Web.Http.OData.Builder;
+
+namespace ProductService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            //var result2 = builder.GetEdmModel();
+            
+           
+            config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+
+
+            // Web API routes
+            //config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+        }
+    }
+}
